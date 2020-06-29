47b38a0ac24ac1225b0de597821dbe50f65fd9b4
anujbidkar <anujbidkar8@gmail.com>
2020-06-29 19:25:44 +0530

    CRUD operation Done

--- myblog/urls.py
+++ myblog/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('',views.Homepage.as_view(),name='home'),
+    path('new',views.AddPost.as_view(),name='new'),
+    path('detailView/<int:pk>',views.ViewDetail.as_view(),name='detailView'),
+    path('blog_edit/<int:pk>',views.EditPost.as_view(),name='blog_edit'),
+    path('blog_delete/<int:pk>',views.DeletePost.as_view(),name='blog_delete'),
 ]
--- myblog/views.py
+++ myblog/views.py
@@ -1,8 +1,30 @@
-from django.views.generic import ListView
-
+from django.views.generic import ListView,DetailView
+from django.views.generic import CreateView,UpdateView,DeleteView
 from .models import Myblog
-
+from django.urls import reverse_lazy
 
 class Homepage(ListView):
     model = Myblog
     template_name ="home.html"
+
+class ViewDetail(DetailView):
+    model = Myblog
+    template_name ="DetailView.html"
+    context_object_name = 'batman'
+
+class AddPost(CreateView):
+    model = Myblog
+    template_name ="addnew.html"
+    fields = ['author','title' ,'text']
+
+class EditPost(UpdateView):
+    model = Myblog
+    template_name ="blog_edit.html"
+    fields = ['title' ,'text']
+
+class DeletePost(DeleteView):
+    model = Myblog
+    template_name ="blog_delete.html"
+    context_object_name = 'batman'
+    fields = ['title' ,'text']
+    success_url = reverse_lazy('home')
